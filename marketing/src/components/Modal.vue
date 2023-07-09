b6548b1be669757af01afbca8ef5f277b2b89077
Austin Zurfluh <azurfluh@bitmischief.io>
2023-07-08 22:19:16 -0700

    get marketing site building

--- marketing/src/components/Modal.vue
+++ marketing/src/components/Modal.vue
@@ -56,7 +56,6 @@
 
 <script setup lang="ts">
 import { XMarkIcon } from "@heroicons/vue/24/outline";
-import { defineEmits } from "vue";
 import {
   Dialog,
   DialogPanel,
--- marketing/src/views/HomeView.vue
+++ marketing/src/views/HomeView.vue
@@ -176,4 +176,3 @@
     </div>
   </div>
 </template>
-<script setup></script>
