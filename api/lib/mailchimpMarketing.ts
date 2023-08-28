6e8318f749757f30953602af8ad7c03790384057
Austin Zurfluh <azurfluh@bitmischief.io>
2023-08-28 14:03:55 -0700

    adding mailchimp secrets properly

--- api/lib/mailchimpMarketing.ts
+++ api/lib/mailchimpMarketing.ts
@@ -1,8 +1,8 @@
 import client from "@mailchimp/mailchimp_marketing";
 
 client.setConfig({
-  apiKey: "44cbd76f5499041373e862bc9113e168-us12",
-  server: "us12",
+  apiKey: process.env.MAILCHIMP_API_KEY as string,
+  server: process.env.MAILCHIMP_SERVER_PREFIX as string,
 });
 
 export default client;
