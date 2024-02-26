e4c67edaf9edf0a71bab6c9edfb861b036fce840
Austin Zurfluh <austin@mythweaver.co>
2024-02-26 11:23:04 -0800

    handle user doesn't exist in intercom yet

--- api/lib/intercom.ts
+++ api/lib/intercom.ts
@@ -1,5 +1,7 @@
 import { Client } from 'intercom-client';
 import { isLocalDevelopment } from './utils';
+import { prisma } from './providers/prisma';
+import { AppError, HttpCode } from './errors/AppError';
 const intercomClient = new Client({
   tokenAuth: { token: process.env.INTERCOM_ACCESS_TOKEN || '' },
 });
@@ -8,7 +10,32 @@ export const setIntercomCustomAttributes = async (
   userId: number,
   attributes: any,
 ) => {
-  if (!isLocalDevelopment) {
+  if (isLocalDevelopment) {
+    return;
+  }
+
+  const contact = await intercomClient.contacts.find({ id: userId.toString() });
+
+  if (!contact) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!user) {
+      throw new AppError({
+        description: 'User not found.',
+        httpCode: HttpCode.NOT_FOUND,
+      });
+    }
+
+    await intercomClient.contacts.createUser({
+      externalId: userId.toString(),
+      email: user.email,
+      customAttributes: attributes,
+    });
+  } else {
     await intercomClient.contacts.update({
       id: userId.toString(),
       customAttributes: attributes,
