7edfeb98a55b9cacf70f3a4655844435dd0a69ca
Jacob Gwin <jacobegwin@gmail.com>
2024-03-25 13:27:48 -0700

    use status instead of message

--- api/lib/intercom.ts
+++ api/lib/intercom.ts
@@ -52,23 +52,23 @@ export const setIntercomCustomAttributes = async (
         customAttributes: attributes,
       });
     } catch (e: any) {
-      if (
-        e.message.startsWith(
-          'A contact matching those details already exists with id=',
-        )
-      ) {
-        const id = e.message.split(
-          'A contact matching those details already exists with id=',
-        )[1];
-        logger.info('Updating intercom contact with id=', id);
-        await intercomClient.contacts.update({
-          id: id,
-          externalId: userId.toString(),
-          customAttributes: attributes,
-        });
-      } else {
-        logger.error('Something went wrong creating intercom contact', e);
+      if (e.status === 409) {
+        const id =
+          e.message.split(
+            'A contact matching those details already exists with id=',
+          )[1] ?? null;
+        if (id) {
+          logger.info('Updating intercom contact with id=', id);
+          await intercomClient.contacts.update({
+            id: id,
+            externalId: userId.toString(),
+            customAttributes: attributes,
+          });
+          return;
+        }
       }
+
+      logger.error('Something went wrong creating intercom contact', e);
     }
   } else {
     await intercomClient.contacts.update({
