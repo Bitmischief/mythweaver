b1bfd881a7699c6f55fc6113fcd6dd4076fd7d9b
Jacob Gwin <jacobegwin@gmail.com>
2024-04-22 17:44:54 -0700

    remove newlines from planning when migrating to planning json

--- api/prisma/migrations/20240422234324_migrate_planning_to_planning_json/migration.sql
+++ api/prisma/migrations/20240422234324_migrate_planning_to_planning_json/migration.sql
@@ -1,3 +1,3 @@
 UPDATE "sessions"
-SET "planningJson" = format('{ "time": 1713553780175, "blocks": [{ "id": "ABCDEFHIJK", "data": { "text": "%s" }, "type": "paragraph" }], "version": "2.29.1" }', planning)::json
+SET "planningJson" = format('{ "time": 1713553780175, "blocks": [{ "id": "ABCDEFHIJK", "data": { "text": "%s" }, "type": "paragraph" }], "version": "2.29.1" }', regexp_replace(planning, E'[\\n\\r]+', ' ', 'g' ))::json
 WHERE "planning" IS NOT NULL AND "planningJson" IS NULL;
