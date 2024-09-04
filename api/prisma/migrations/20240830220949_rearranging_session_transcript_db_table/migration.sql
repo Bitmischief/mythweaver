c0a675bd6400baa107ded8ec23ffff38c68ae161
Austin Zurfluh <austin@mythweaver.co>
2024-09-04 15:46:40 -0700

    modify session status add migration

--- api/prisma/migrations/20240830220949_rearranging_session_transcript_db_table/migration.sql
+++ api/prisma/migrations/20240830220949_rearranging_session_transcript_db_table/migration.sql
@@ -11,12 +11,15 @@
 CREATE TYPE "TranscriptStatus" AS ENUM ('PROCESSING', 'COMPLETE', 'FAILED');
 
 -- AlterTable
-ALTER TABLE "session_transcriptions" ADD COLUMN     "status_new" "TranscriptStatus" NOT NULL,
-ADD COLUMN     "transcript" JSONB,
-ADD COLUMN     "transcriptExternalId" TEXT,
-ADD COLUMN     "transcriptSpeakerMapping" JSONB;
+ALTER TABLE "session_transcriptions"
+ADD COLUMN "status_new" "TranscriptStatus" NOT NULL DEFAULT 'COMPLETE',
+ADD COLUMN "transcript" JSONB,
+ADD COLUMN "transcriptExternalId" TEXT,
+ADD COLUMN "transcriptSpeakerMapping" JSONB;
 
 -- AlterTable
-ALTER TABLE "sessions" DROP COLUMN "transcript",
+ALTER TABLE "sessions"
+DROP COLUMN "transcript",
 DROP COLUMN "transcriptExternalId",
 DROP COLUMN "transcriptSpeakerMapping";
+
