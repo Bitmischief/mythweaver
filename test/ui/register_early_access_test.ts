c146128de588ee5b9586c5af3c6d3fd7bf1e04e5
Austin Zurfluh <austin@mythweaver.co>
2024-09-16 14:30:43 -0700

    remove test for now

--- test/ui/register_early_access_test.ts
+++
@@ -1,8 +0,0 @@
-Feature('Magic Link');
-
-Scenario('Send myself a magic link with valid e-mail', ({ I, loginPage }) => {
-  loginPage.login('Andrew.S.Lobdell@gmail.com');
-  I.click('Back to login');
-  I.see('Welcome to MythWeaver');
-});
-
