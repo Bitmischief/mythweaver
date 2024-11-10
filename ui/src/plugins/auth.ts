43332de524c976cbcb3a30f4162d41814082cc0e
Austin Zurfluh <azurfluh@bitmischief.io>
2024-11-09 20:33:45 -0800

    handle 401 errors better

--- ui/src/plugins/auth.ts
+++ ui/src/plugins/auth.ts
@@ -1,6 +1,7 @@
 import { createAuth0 } from '@auth0/auth0-vue';
 import type { Auth0Plugin } from '@auth0/auth0-vue';
 import { useAuthStore } from '@/store';
+import { useRoute } from 'vue-router';
 
 const auth0Client: Auth0Plugin = createAuth0({
   domain: import.meta.env.VITE_AUTH0_DOMAIN,
@@ -17,6 +18,9 @@ export default auth0Client;
 
 export const logout = async () => {
   const authStore = useAuthStore();
+  const route = useRoute();
+  const currentPath = route.path;
+
   await authStore.logout();
   await auth0Client.logout();
   await auth0Client.loginWithRedirect({
@@ -24,17 +28,11 @@ export const logout = async () => {
       screen_hint: 'login',
     },
     appState: {
-      target: `/conjure`,
+      target: currentPath,
     },
   });
 };
 
-export const getAccessToken = async () => {
-  let token = null;
-  try {
-    token = await auth0Client.getAccessTokenSilently();
-  } catch (e) {
-    console.log('No access token');
-  }
-  return token;
+export const getAccessToken = async (options?: { cacheMode?: 'on' | 'off' }) => {
+  return await auth0Client.getAccessTokenSilently(options);
 };
