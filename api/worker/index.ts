e0be491821a2b08b5e306d0493308d84a5857329
Austin Zurfluh <austin@mythweaver.co>
2024-12-12 10:23:22 -0800

    remove now unused image status queue

--- api/worker/index.ts
+++ api/worker/index.ts
@@ -1,7 +1,6 @@
 import { processTagsQueue } from './jobs/processTags';
 import { conjureQueue } from './jobs/conjure';
 import { endTrialQueue } from './jobs/endTrials';
-import { checkImageStatusQueue } from './jobs/imageStatus';
 import { indexCampaignContextQueue } from './jobs/indexCampaignContext';
 import { sessionTranscriptionQueue } from './jobs/transcribeSession';
 import { dailyCampaignContextQueue } from './jobs/dailyCampaignContextSync';
@@ -12,7 +11,6 @@ export {
   processTagsQueue,
   conjureQueue,
   endTrialQueue,
-  checkImageStatusQueue,
   indexCampaignContextQueue,
   sessionTranscriptionQueue,
   dailyCampaignContextQueue,
