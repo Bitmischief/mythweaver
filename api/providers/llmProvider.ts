e910ef32079314dd791f6a83ff22654dc7acf16b
Austin Zurfluh <austin@mythweaver.co>
2024-12-16 15:42:30 -0800

    refactor generate text to new llm provider

--- api/providers/llmProvider.ts
+++ api/providers/llmProvider.ts
@@ -1,43 +1,7 @@
-import { TextContentBlock } from 'openai/resources/beta/threads';
-import { getCampaign, getCampaignContextConfig } from '../dataAccess/campaigns';
-import { AppError, HttpCode } from '../lib/errors/AppError';
-import { getClient } from '../lib/providers/openai';
+import { OpenAIProvider } from './llms/openAIProvider';
 
-export class LLMProvider {
-  private openai;
-
-  constructor() {
-    this.openai = getClient();
-  }
-
-  async generateText(campaignId: number, prompt: string): Promise<string> {
-    const campaign = await getCampaign(campaignId);
-
-    if (!campaign) {
-      throw new AppError({
-        description: 'Campaign not found.',
-        httpCode: HttpCode.BAD_REQUEST,
-      });
-    }
-
-    const contextConfig = await getCampaignContextConfig(campaignId);
-
-    const thread = await this.openai.beta.threads.create();
-
-    await this.openai.beta.threads.messages.create(thread.id, {
-      content: prompt,
-      role: 'assistant',
-    });
-
-    const run = await this.openai.beta.threads.runs.createAndPoll(thread.id, {
-      assistant_id: contextConfig.assistantId,
-    });
-
-    const messages = await this.openai.beta.threads.messages.list(thread.id, {
-      run_id: run.id,
-    });
-
-    const response = messages.data.pop();
-    return (response?.content[0] as TextContentBlock)?.text?.value || '';
-  }
+export interface LLMProvider {
+  generateText(campaignId: number, prompt: string): Promise<string>;
 }
+
+export const defaultLLMProvider = new OpenAIProvider() as LLMProvider;
